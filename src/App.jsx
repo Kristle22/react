85fe0cf6323af8956dacfc156316f5e49fa40a45
Kristina <crislayn@yahoo.com>
2022-06-17 20:38:23 +0300

    removing empty folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,114 +1,23 @@
-import { useEffect, useReducer } from 'react';
+import { useState } from 'react';
 import './App.scss';
-import axios from 'axios';
-import bookReducer from './Reducers/bookReducer';
-import bookTypeReducer from './Reducers/bookTypeReducer';
+import Suo from './Components/024/Suo';
+import Duomenys from './Contexts/Duomenys';
 
 function App() {
-  // const [list, setList] = useState([]);
-
-  // useEffect(() => {
-  //   axios.get('http://in3.dev/knygos/').then((res) => setList(res.data));
-  // }, []);
-
-  const [books, dispachBooks] = useReducer(bookReducer, []);
-  const [types, dispachTypes] = useReducer(bookTypeReducer, []);
-
-  // const [reload, setReload] = useReducer(reloadReducer, []);
-
-  useEffect(() => {
-    axios.get('http://in3.dev/knygos/').then((res) => {
-      const action = {
-        type: 'book_list',
-        payload: res.data,
-      };
-
-      dispachBooks(action);
-    });
-  }, []);
-  console.log(books);
-
-  useEffect(() => {
-    axios.get('https://in3.dev/knygos/types/').then((res) => {
-      localStorage.setItem('book_type', JSON.stringify(res.data));
-      const category = JSON.parse(localStorage.getItem('book_type'));
-      console.log('category', category);
-      const action = {
-        type: 'book_type',
-        payload: category,
-      };
-      dispachTypes(action);
-    });
-  }, []);
-
-  console.log('types', books);
-
-  const sortBooks = () => {
-    const action = {
-      type: 'sort',
-    };
-    dispachBooks(action);
-  };
-  const defSort = () => {
-    const action = {
-      type: 'def_sort',
-    };
-    dispachBooks(action);
-  };
-
-  const priceFilter = () => {
-    const action = {
-      type: 'price_filter',
-    };
-    dispachBooks(action);
-  };
-
-  const reload = () => {
-    axios.get('https://in3.dev/knygos/types/').then((_) => {
-      const action = {
-        type: 'reload',
-        payload: books,
-      };
-      dispachBooks(action);
-    });
-    axios.get('https://in3.dev/knygos/types/').then((_) => {
-      const action = {
-        type: 'reload',
-        payload: types,
-      };
-      dispachTypes(action);
-    });
-  };
+  const [counter1, setCounter1] = useState(1);
+  const [counter2, setCounter2] = useState(3);
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1>REDUCER</h1>
-        <h2>Book list</h2>
-        {books.length ? (
-          books.map((el) =>
-            el.show ? (
-              <div key={el.id}>
-                <b>'{el.title}'</b>: {el.price} EUR.{' '}
-                <i>
-                  {types.map((type) =>
-                    type.id === el.type ? type.title : null
-                  )}
-                </i>
-              </div>
-            ) : null
-          )
-        ) : (
-          <div className='lds-hourglass'></div>
-        )}
-        <div>
-          <button onClick={sortBooks}>sort Books</button>
-          <button onClick={defSort}>defSort Books</button>
-          <button onClick={priceFilter}>More than 13 Eur.</button>
-          <button onClick={reload}>Reload</button>
-        </div>
-      </header>
-    </div>
+    <Duomenys.Provider value={{ counter1, counter2 }}>
+      <div className='App'>
+        <header className='App-header'>
+          <h1>CONTEXT</h1>
+          <Suo />
+          <button onClick={() => setCounter1((c) => c + 1)}>PLUS 1</button>
+          <button onClick={() => setCounter2((c) => c + 3)}>PLUS 3</button>
+        </header>
+      </div>
+    </Duomenys.Provider>
   );
 }
 export default App;
---
+++ src/App_books.jsx
@@ -0,0 +1,114 @@
+import { useEffect, useReducer } from 'react';
+import './App.scss';
+import axios from 'axios';
+import bookReducer from './Reducers/bookReducer';
+import bookTypeReducer from './Reducers/bookTypeReducer';
+
+function App() {
+  // const [list, setList] = useState([]);
+
+  // useEffect(() => {
+  //   axios.get('http://in3.dev/knygos/').then((res) => setList(res.data));
+  // }, []);
+
+  const [books, dispachBooks] = useReducer(bookReducer, []);
+  const [types, dispachTypes] = useReducer(bookTypeReducer, []);
+
+  // const [reload, setReload] = useReducer(reloadReducer, []);
+
+  useEffect(() => {
+    axios.get('http://in3.dev/knygos/').then((res) => {
+      const action = {
+        type: 'book_list',
+        payload: res.data,
+      };
+
+      dispachBooks(action);
+    });
+  }, []);
+  console.log(books);
+
+  useEffect(() => {
+    axios.get('https://in3.dev/knygos/types/').then((res) => {
+      localStorage.setItem('book_type', JSON.stringify(res.data));
+      const category = JSON.parse(localStorage.getItem('book_type'));
+      console.log('category', category);
+      const action = {
+        type: 'book_type',
+        payload: category,
+      };
+      dispachTypes(action);
+    });
+  }, []);
+
+  console.log('types', books);
+
+  const sortBooks = () => {
+    const action = {
+      type: 'sort',
+    };
+    dispachBooks(action);
+  };
+  const defSort = () => {
+    const action = {
+      type: 'def_sort',
+    };
+    dispachBooks(action);
+  };
+
+  const priceFilter = () => {
+    const action = {
+      type: 'price_filter',
+    };
+    dispachBooks(action);
+  };
+
+  const reload = () => {
+    axios.get('https://in3.dev/knygos/types/').then((_) => {
+      const action = {
+        type: 'reload',
+        payload: books,
+      };
+      dispachBooks(action);
+    });
+    axios.get('https://in3.dev/knygos/types/').then((_) => {
+      const action = {
+        type: 'reload',
+        payload: types,
+      };
+      dispachTypes(action);
+    });
+  };
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>REDUCER</h1>
+        <h2>Book list</h2>
+        {books.length ? (
+          books.map((el) =>
+            el.show ? (
+              <div key={el.id}>
+                <b>'{el.title}'</b>: {el.price} EUR.{' '}
+                <i>
+                  {types.map((type) =>
+                    type.id === el.type ? type.title : null
+                  )}
+                </i>
+              </div>
+            ) : null
+          )
+        ) : (
+          <div className='lds-hourglass'></div>
+        )}
+        <div>
+          <button onClick={sortBooks}>sort Books</button>
+          <button onClick={defSort}>defSort Books</button>
+          <button onClick={priceFilter}>More than 13 Eur.</button>
+          <button onClick={reload}>Reload</button>
+        </div>
+      </header>
+    </div>
+  );
+}
+export default App;
